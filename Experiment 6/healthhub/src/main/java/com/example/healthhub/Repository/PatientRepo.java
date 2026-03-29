9d2041903527934c41e70cd51c53246382717dbd
nav33n03 <nav33nkumar03@gmail.com>
2026-03-29 11:20:20 +0530

    Delete Experiment 6/healthhub directory

--- Experiment 6/healthhub/src/main/java/com/example/healthhub/Repository/PatientRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.healthhub.Repository;
-import com.example.healthhub.Entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  PatientRepo extends JpaRepository<Patient, Long> {
-    
-}
